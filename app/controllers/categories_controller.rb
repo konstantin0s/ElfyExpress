67871aa39559de4f34504e5a24f2cd59125867ae
opaismoe <wiebe.buursink@gmail.com>
2017-12-19 17:23:04 +0100

    Add routes, Add controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,10 @@
 class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @products = @category.products
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
+
+
+  resources :users do
+    resources :purchases, only: [:show, :edit, :update]
+    resources :profiles, only: [:new, :edit, :create, :update]
+  end
+  resources :categories do
+    resources :products
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
