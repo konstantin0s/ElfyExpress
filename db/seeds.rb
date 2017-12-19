2a59f08c27ea57f0474df127c0a68560afae41c5
MoCodeGitHub <mhadji22@gmail.com>
2017-12-19 17:32:42 +0100

    Add Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Profile.destroy_all
+Selections.destroy_all
+User.destroy_all
+Category.destroy_all
+Purchases.destroy_all
+Products.destroy_all
+
+#users
+users << User.create(
+      email: []
+      password: []
+    )
+end
+
+#categories
+categories = []
+categories << Category.create!(name: "Ornaments")
+categories << Category.create!(name: "Lights")
+categories << Category.create!(name: "Trees")
+categories << Category.create!(name: "Tree Stands")
+categories << Category.create!(name: "Kittens")
+categories << Category.create!(name: "Garland"
