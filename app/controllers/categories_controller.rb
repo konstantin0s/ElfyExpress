cb82c363610338077f28d08c84999c81508baf3e
Abohte <adinda.bohte@gmail.com>
2017-12-22 13:50:10 +0100

    Fix navbar, fix categories_controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update]
 
-  def index
-    @categories = Category.all
-  end
-
   def show
     @products = @category.products
   end
