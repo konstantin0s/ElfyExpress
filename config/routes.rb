dd6d126434d0ec3380ca6a27ce7147c5cc2183ad
Abohte <adinda.bohte@gmail.com>
2017-12-22 15:34:37 +0100

    Add product show page with buy button

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,12 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :purchases, only: [:show, :edit, :update]
+
   resources :profiles, only: [:new, :edit, :create, :update]
 
   resources :categories, only: [:show]
-  resources :products
+  resources :products do
+    resources :selections
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
