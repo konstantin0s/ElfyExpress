6461b31dc940ecc5878edc3ae037c8bb1afc4b25
opaismoe <wiebe.buursink@gmail.com>
2017-12-22 10:50:40 +0100

    Put back the validations for profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,9 @@ class Profile < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :user, presence: true
-
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :postal_code, presence: true
 
   def full_name
       "#{first_name} #{last_name}"
